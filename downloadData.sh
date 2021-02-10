654980fbd682bd5812e0f93f4ed292a6e56227b3
Petra Heřmánková <petra.janouchova@gmail.com>
2021-02-10 11:03:46 +0100

    updated with cities

--- downloadData.sh
+++ downloadData.sh
@@ -3,3 +3,5 @@
 wget -m http://awmc.unc.edu/awmc/map_data/shapefiles/ba_roads/
 wget -m http://awmc.unc.edu/awmc/map_data/shapefiles/cultural_data/political_shading/roman_empire_ad_117/shape/
 wget -m http://awmc.unc.edu/awmc/map_data/shapefiles/strabo_data/
+wget -m http://oxrep.classics.ox.ac.uk/oxrep/docs/Hanson2016/Hanson2016_Cities_OxREP.csv
+
