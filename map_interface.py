c8ed31ebf3a9493828df32bc1427397b67496dda
Brian Ballsun-Stanton <denubis@noreply.users.github.com>
2021-04-06 17:07:48 +1000

    I wonder if this will work?

--- map_interface.py
+++ map_interface.py
@@ -8,14 +8,41 @@ import shutil
 import datetime
 import glob
 
+
+import rasterio as rio
+from rasterio.warp import calculate_default_transform, reproject, Resampling
+
+#https://www.earthdatascience.org/courses/scientists-guide-to-plotting-data-in-python/plot-spatial-data/customize-raster-plots/interactive-maps/
+
+
+
 class Parseargs:
     maps = None
 
 def make_map_interface():
     args = Parseargs()
     map_button = widgets.Button(description="Generate New Maps!")
+    map_button_interactive = widgets.Button(description="Reload Interactive!")
     out = widgets.Output(layout={'border': '1px solid black'})
-    display(HTML("<h1>Mapper</h1>"), map_button, out)
+    display(HTML("<h1>Interactive Map</h1>"))
+    display(HTML("<h2>Choose datafiles to plot</h2>"))
+    display(HTML("<h2>Map viewer</h2>"), map_button_interactive)
+    
+    def interactive_refresh(b):
+        # https://stackoverflow.com/a/38797877
+        LDN_COORDINATES = (51.5074, 0.1278) 
+        m = folium.Map(location=LDN_COORDINATES, zoom_start=12)
+        #m._build_map()
+        #mapWidth, mapHeight = (400,500) # width and height of the displayed iFrame, in pixels
+        #srcdoc = m.HTML.replace('"', '&quot;')
+        #embed = HTML('<iframe srcdoc="{}" '
+        #             'style="width: {}px; height: {}px; display:block; width: 50%; margin: 0 auto; '
+        #             'border: none"></iframe>'.format(srcdoc, width, height))
+        display(m)
+        
+    
+    
+    display(HTML("<h1>Generate PDF Map</h1>"), map_button, out)
     def map_on_button_clicked(b):
         with out:
             display(HTML("<p>Starting Map Generation</p>"))
@@ -35,3 +62,4 @@ def make_map_interface():
                 display(HTML(f"<li><a href='{zipfile}'>{zipfile}</a></li>"))
             display(HTML("</ul>"))
     map_button.on_click(map_on_button_clicked)
+    map_button_interactive.on_click(interactive_refresh)
