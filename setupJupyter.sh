3080356fa3dc4c1f68522e6ec2189e97ccfa86ac
Brian Ballsun-Stanton <denubis@noreply.users.github.com>
2021-07-31 22:43:25 +1000

    passes local docker...

---
+++ setupJupyter.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "nbextension"
+jupyter nbextension enable --py widgetsnbextension --sys-prefix 
+echo "contrib"
+jupyter contrib nbextension install --sys-prefix 
+echo "init_Cell"
+jupyter nbextension enable init_cell/main 
+echo "dashboards"
+#jupyter labextension install @voila-dashboards/jupyterlab-preview 
+echo "serverextension"
+jupyter serverextension enable voila --sys-prefix
