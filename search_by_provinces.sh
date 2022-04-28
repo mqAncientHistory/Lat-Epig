5d0a92001082dda2706e4a0279b6ea85e5e6f943
Brian Ballsun-Stanton <denubis@noreply.users.github.com>
2022-04-28 18:52:10 +1000

    docker to full scrape
    Signed-off-by: Brian Ballsun-Stanton <denubis@noreply.users.github.com>

--- search_by_provinces.sh
+++ search_by_provinces.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-pip install -r requirements.txt
-python3 -m pip install --no-cache-dir --editable .
+#pip install -r requirements.txt
+#python3 -m pip install --no-cache-dir --editable .
 
 for province in "Achaia" "Aegyptus" "Africa proconsularis" "Alpes Cottiae" "Alpes Graiae" "Alpes Maritimae" "Alpes Poeninae" "Apulia et Calabria / Regio II" "Aquitani(c)a" "Arabia" "Armenia" "Asia" "Baetica" "Barbaricum" "Belgica" "Britannia" "Bruttium et Lucania / Regio III" "Cappadocia" "Cilicia" "Corsica" "Creta et Cyrenaica" "Cyprus" "Dacia" "Dalmatia" "Etruria / Regio VII" "Galatia" "Gallia Narbonensis" "Germania inferior" "Germania superior" "Hispania citerior" "Italia" "Latium et Campania / Regio I" "Liguria / Regio IX" "Lugudunensis" "Lusitania" "Lycia et Pamphylia" "Macedonia" "Mauretania Caesariensis" "Mauretania Tingitana" "Mesopotamia" "Moesia inferior" "Moesia superior" "Noricum" "Numidia" "Palaestina" "Pannonia inferior" "Pannonia superior" "Picenum / Regio V" "Pontus et Bithynia" "Provincia incerta" "Raetia" "Regnum Bospori" "Roma" "Aemilia / Regio VIII" "Samnium / Regio IV" "Sardinia" "Sicilia" "Syria" "Thracia" "Transpadana / Regio XI" "Umbria / Regio VI" "Venetia et Histria / Regio X"
 	do
